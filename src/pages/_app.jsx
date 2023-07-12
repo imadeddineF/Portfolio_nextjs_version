7997d4190682466352765196fc3df7137456c2fe
Imad Eddine Fillali <imadeddinefillaliusthb@gmail.com>
2023-07-12 15:34:00 +0100

    test

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,15 @@
-import "@/styles/globals.css";
-import { Montserrat } from "next/font/google";
-import Head from "next/head";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-import { useRouter } from "next/router";
-import { AnimatePresence } from "framer-motion";
+import '@/styles/globals.css';
+import { Montserrat } from 'next/font/google';
+import Head from 'next/head';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
+import { useRouter } from 'next/router';
+import { AnimatePresence } from 'framer-motion';
 
 // add the font familly
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-mont",
+  subsets: ['latin'],
+  variable: '--font-mont',
 });
 
 export default function App({ Component, pageProps }) {
@@ -22,11 +22,14 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       {/* the main app */}
       <main
-        className={`${montserrat.variable} font-mont dark:bg-dark bg-light w-full min-h-screen `}>
+        className={`${montserrat.variable} font-mont dark:bg-dark bg-light w-full min-h-screen `}
+      >
         {/* add the navbar as component */}
         <Navbar />
+
         {/* Add the animate presence for add the page transition */}
         <AnimatePresence mode="wait">
           <Component key={router.asPath} {...pageProps} />
